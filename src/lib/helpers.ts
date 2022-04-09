ad6bc5e97a20c65484caa58ae335422ea0370688
Egor O. Chindyasov <egor@chindyasov.ru>
2022-04-09 17:13:27 +0300

    added getear method

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -3,3 +3,8 @@ import {valuta} from '../constants/index'
 export const getMoneyFormat = (num: number) => {
     return `${new Intl.NumberFormat().format(num)} ${valuta}`
 }
+
+export const getYear = (num: number) => {
+    return num == 1 ? 'год' :
+    num < 5 ? 'года' : 'лет'
+}
