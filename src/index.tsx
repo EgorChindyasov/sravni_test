4cf036b1c4520a8022d8a0ea227c84d9a43fe996
Egor O. Chindyasov <egor@chindyasov.ru>
2022-04-09 18:30:26 +0300

    used a new functional of react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
 import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
@@ -8,11 +8,11 @@ import {store} from './redux/store'
 import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
+const container = createRoot(document.getElementById('root'))
+container.render(
 	<BrowserRouter>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</BrowserRouter>,
-  document.getElementById('root')
+	</BrowserRouter>
 )
