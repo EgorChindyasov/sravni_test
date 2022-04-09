31650b46d50e81bc5c21527946fa9565dfda3852
Egor O. Chindyasov <egor@chindyasov.ru>
2022-04-09 21:44:54 +0300

    removed arrays of options for selecton in navbar

--- src/redux/filterReducer.ts
+++ src/redux/filterReducer.ts
@@ -1,5 +1,3 @@
-import {InformBlock} from '../data/types'
-
 export type FilterStatesType = {
 	chosenTerm: number
 	chosenTargetMortage: string
@@ -7,34 +5,16 @@ export type FilterStatesType = {
 
 export type FilterActionType = {
 	type: string
-	value?: InformBlock[]
 	payload?: number | string
 }
 
 export const defaultStates = {
-	termOptions: [],
-	targetMortgageOptions: [],
 	chosenTerm: 0,
 	chosenTargetMortage: 'all',
 }
 
 export const filterReducer = (state = defaultStates, action: FilterActionType) => {
   switch (action.type) {
-		case 'SET_OPTIONS':
-
-			const dataTermSet: Set<number> = new Set()
-			const dataNameSet: Set<string> = new Set()
-
-			action.value.map(({rate, name}) => {
-				dataTermSet.add(rate.periods[0].term.to / 12)
-				dataNameSet.add(name)
-			})
-
-			return {
-				...state, 
-				termOptions: Array.from(dataTermSet), 
-				targetMortgageOptions: Array.from(dataNameSet)
-			}
 		case 'CHOSE_TERM': 
 			return {...state, chosenTerm: action.payload}
 		case 'CHOSE_TARGET_MORTAGE':
