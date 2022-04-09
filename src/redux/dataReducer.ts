836df80a9837e57247f5b5f4b3831b652749f070
Egor O. Chindyasov <egor@chindyasov.ru>
2022-04-09 21:45:25 +0300

    added arrays of options for selecton in navbar

--- src/redux/dataReducer.ts
+++ src/redux/dataReducer.ts
@@ -3,6 +3,8 @@ import {InformBlock} from '../data/types'
 export type DataStatesType = {
 	data: InformBlock
 	isLoaded: boolean
+	termOptions: number[]
+	targetMortgageOptions: string[]
 }
 
 export type DataActionType = {
@@ -12,16 +14,28 @@ export type DataActionType = {
 
 export const defaultStates = {
 	isLoaded: false,
-	data: []
+	data: [],
+	termOptions: [],
+	targetMortgageOptions: [],
 }
 
 export const dataReducer = (state = defaultStates, action: DataActionType) => {
   switch (action.type) {
 		case 'SET':
+			const dataTermSet: Set<number> = new Set()
+			const dataNameSet: Set<string> = new Set()
+
+			action.value.map(({rate, name}) => {
+				dataTermSet.add(rate.periods[0].term.to / 12)
+				dataNameSet.add(name)
+			})
+
 			return {
 				...state, 
 				data: action.value,
-				isLoaded: true
+				isLoaded: true,
+				termOptions: Array.from(dataTermSet), 
+				targetMortgageOptions: Array.from(dataNameSet)
 			}
 		default: 
 			return state
