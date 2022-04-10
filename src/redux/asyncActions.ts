f4be02d57b44cfc554abe7990047cc5bd4a98eba
Egor O. Chindyasov <61084575+EgorChindyasov@users.noreply.github.com>
2022-04-11 00:48:04 +0300

    Update asyncActions.ts

--- src/redux/asyncActions.ts
+++ src/redux/asyncActions.ts
@@ -4,7 +4,7 @@ export const fetchData = () => {
     return async(dispatch) => {
         try {
             const response = await axios('http://localhost:5000/')
-		    dispatch({type: 'SET', value: response.data.data})
+	    dispatch({type: 'SET', value: response.data.data})
         } catch(error) {
             console.log('Error:', error)
         }
