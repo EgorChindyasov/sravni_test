a670e638309f140f0477e9198e22eaf28705c754
Egor O. Chindyasov <egor@chindyasov.ru>
2022-04-11 00:45:35 +0300

    added try-catch block

--- src/redux/asyncActions.ts
+++ src/redux/asyncActions.ts
@@ -2,7 +2,11 @@ import axios from 'axios'
 
 export const fetchData = () => {
     return async(dispatch) => {
-        const response = await axios('http://localhost:5000/')
-		dispatch({type: 'SET', value: response.data.data})
+        try {
+            const response = await axios('http://localhost:5000/')
+		    dispatch({type: 'SET', value: response.data.data})
+        } catch(error) {
+            console.log('Error:', error)
+        }
     }
 } 
